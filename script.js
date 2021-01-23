dcff50d7fff45f5830f9c1c9fc69fe256a0ae753
Simon <simonrosenberg1@gmail.com>
2021-01-23 09:31:48 +0000

    Add functionality to switch between projects in projects.html

---
+++ script.js
@@ -0,0 +1,36 @@
+// create function to flip through projects on projects.html
+
+let tabsArray = document.getElementsByClassName('tab');
+
+for (let i = 0; i < tabsArray.length; i++) {
+
+  tabsArray[i].addEventListener('click', function() {
+
+    if (event.target.classList.contains('unselected')) {
+
+      document.getElementsByClassName('selected')[0].classList.add('unselected');
+      document.getElementsByClassName('selected')[0].classList.remove('selected');
+      event.target.classList.add('selected');
+      event.target.classList.remove('unselected');
+    }
+
+    document.getElementsByClassName('visible')[0].classList.add('hidden-info');
+    document.getElementsByClassName('visible')[0].classList.remove('visible');
+
+    switch (event.target.id) {
+      case 'tab1':
+        document.getElementById('info1').classList.remove('hidden-info');
+        document.getElementById('info1').classList.add('visible');
+        break;
+      case 'tab2':
+        document.getElementById('info2').classList.remove('hidden-info');
+        document.getElementById('info2').classList.add('visible');
+        break;
+      case 'tab3':
+        document.getElementById('info3').classList.remove('hidden-info');
+        document.getElementById('info3').classList.add('visible');
+        break;
+    }
+
+  })
+}
